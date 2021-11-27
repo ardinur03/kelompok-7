b2fa2d3e5ee53c02444102279deea23c89f582ac
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2021-11-27 14:55:21 +0700

    FEAT : create feature about animals

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 const menuToggle = document.querySelector(".menu-toggle .input");
-const nav = document.querySelector("nav ul");
+const navUl = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
+const navLink = document.querySelector("nav ul li .nav-link");
 
 menuToggle.addEventListener("click", () => {
-    nav.classList.toggle("slide");
+    navUl.classList.toggle("slide");
+});
+
+// create event listener if scroll is 250 add class to nav element
+window.addEventListener("scroll", (e) => {
+    let scroll = this.scrollY;
+    if (scroll > 680) {
+        // remove class to element
+        nav.className = "nav-bg-primary";
+    } else {
+        // add class to element
+        nav.className = "";
+    }
 });
