c0e46373da64bf8347488ab778f625c6bfe400dd
Muhamad Ardi Nur Insan <ardinurinsan03@gmail.com>
2021-11-30 15:36:07 +0700

    FIX : logo - dropdown

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 const menuToggle = document.querySelector(".menu-toggle .input");
 const navUl = document.querySelector("nav ul");
 const nav = document.querySelector("nav");
-
+const logo = document.querySelector(".logo img");
 const linkBeranda = document.querySelector(".beranda");
-const linkTentang = document.querySelector(".tentang");
+const linkTentang = document.querySelector(".kelompok");
 const linkKontak = document.querySelector(".kontak");
 
 menuToggle.addEventListener("click", () => {
@@ -14,11 +14,13 @@ menuToggle.addEventListener("click", () => {
 window.addEventListener("scroll", (e) => {
     let scroll = this.scrollY;
     console.log(scroll);
-    if (!scroll) {
+    if (!scroll || scroll < 0) {
         // add class active link to nav link
         linkBeranda.className = "active";
     } else {
         if (scroll > 680) {
+            // size logo
+            logo.className = "logo-small";
             // add class to element
             nav.className = "nav-bg-primary";
 
@@ -42,6 +44,9 @@ window.addEventListener("scroll", (e) => {
             // remove class active link to nav link
             linkBeranda.className = "";
         } else {
+            // remove class logo-small
+            logo.className = "";
+
             // remove if up scroll
             // remove class to element
             nav.className = "";
@@ -50,4 +55,14 @@ window.addEventListener("scroll", (e) => {
             linkTentang.className = "";
         }
     }
+});
+
+new Typed("#text-hero", {
+    strings: [
+        "Selamat Datang di Website Dunia Fauna",
+        "Kami menyediakan berbagai macam Kelompok Fauna",
+    ],
+    typeSpeed: 150,
+    delaySpeed: 900,
+    loop: true,
 });
